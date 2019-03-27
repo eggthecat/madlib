18ab1cd22b094b103e0ea1a3cbedc56d1c3fb8fd
Dominic Montelongo and Stuart McKay <dominic@>
2019-03-27 13:02:42 -0700

    to be continued

--- js/scripts.js
+++ js/scripts.js
@@ -1,18 +1,24 @@
-// $(document).ready(function() {
-//   $("#formOne").submit(function(event) {
-//     $(".person1").append("bumperson1");
-//
-//     event.preventDefault();
-//   });
-// });
-
 $(document).ready(function() {
   $("#formOne").submit(function(event) {
-    $(".person1").append("blah blah");
-    $(".person2").append("blah blah");
+    var person1input = $("input#person1").val();
+    var person2input = $("input#person2").val();
+
+    $("#person1").text(person1Input);
+    $("#person2").text(person2Input);
 
     $(".story").show();
 
     event.preventDefault();
   });
 });
+
+// $(document).ready(function() {
+//   $("#formOne").submit(function(event) {
+//     $(".person1").append("blah blah");
+//     $(".person2").append("blah blah");
+//
+//     $(".story").show();
+//
+//     event.preventDefault();
+//   });
+// });
